b423bececa73ea292c6f198da0c9202ec84aa81b
Pascal Sauer <email@cubexs.dev>
2023-12-03 17:00:24 +0100

    Add Project 2

---
+++ Day 20 - Day 22 [Project 2]/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Pascal Sauer on 02.12.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
