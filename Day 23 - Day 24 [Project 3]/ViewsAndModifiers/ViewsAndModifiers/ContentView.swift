afb2c330bd3c4fdfa6bb50cfd5ddfc695428603c
Pascal Sauer <email@cubexs.dev>
2023-12-05 17:16:10 +0100

    Add Project 3

---
+++ Day 23 - Day 24 [Project 3]/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Pascal Sauer on 05.12.23.
+//
+
+import SwiftUI
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        GridStack(rows: 4, columns: 4) {row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
