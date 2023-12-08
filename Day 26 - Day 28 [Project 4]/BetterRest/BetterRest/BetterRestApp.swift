2b791823cbd65a7c410b2ee832021b3063d9ac91
Pascal Sauer <email@cubexs.dev>
2023-12-08 17:05:16 +0100

    Create project 4

---
+++ Day 26 - Day 28 [Project 4]/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Pascal Sauer on 08.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 26 - Day 28 [Project 4]/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Pascal Sauer on 08.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    ContentView()
+}
