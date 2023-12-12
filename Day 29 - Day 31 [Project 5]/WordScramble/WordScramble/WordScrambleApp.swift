fd75e0d57928f7843c915bd30e06d68789f8db56
Pascal Sauer <email@cubexs.dev>
2023-12-12 14:25:47 +0100

    Update project 5

---
+++ Day 29 - Day 31 [Project 5]/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Pascal Sauer on 11.12.23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
