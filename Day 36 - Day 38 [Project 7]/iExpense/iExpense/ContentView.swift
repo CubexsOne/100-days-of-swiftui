efb3d31a0832a54fdc47f84fd52652b5dc07cb47
Pascal Sauer <email@cubexs.dev>
2023-12-18 17:55:19 +0100

    Create project 7 basis app

---
+++ Day 36 - Day 38 [Project 7]/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Pascal Sauer on 18.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 36 - Day 38 [Project 7]/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Pascal Sauer on 18.12.23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
