bcba0481d4e5c1bfa54ed9498ff04aa4fbcbab03
Pascal Sauer <email@cubexs.dev>
2024-01-18 18:37:50 +0100

    Add basics for project 10

---
+++ Day 49 - Day 52 [Project 10]/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,19 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Pascal Sauer on 18.01.24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
---
+++ Day 49 - Day 52 [Project 10]/CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Pascal Sauer on 18.01.24.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
