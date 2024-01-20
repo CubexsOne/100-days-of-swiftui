66bfe92ecabf9c167c4446a01d0a6654fd30ffe5
Pascal Sauer <email@cubexs.dev>
2024-01-20 18:31:42 +0100

    Add alerts and update validation

--- Day 49 - Day 52 [Project 10]/CupcakeCorner/CupcakeCorner/Order.swift
+++ Day 49 - Day 52 [Project 10]/CupcakeCorner/CupcakeCorner/Order.swift
@@ -7,8 +7,26 @@
 
 import Foundation
 
+extension String {
+    func isTrimmedEmpty() -> Bool {
+        self.trimmingCharacters(in: .whitespaces).isEmpty
+    }
+}
+
 @Observable
-class Order {
+class Order: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _streetAddress = "streetAddress"
+        case _city = "city"
+        case _zip = "zip"
+    }
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
     var type = 0
@@ -24,4 +42,33 @@ class Order {
     }
     var extraFrosting = false
     var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isTrimmedEmpty() || streetAddress.isTrimmedEmpty() || city.isTrimmedEmpty() || zip.isTrimmedEmpty() {
+            return false
+        }
+        
+        return true
+    }
+    
+    var cost: Decimal {
+        var cost = Decimal(quantity) * 2
+        
+        cost += Decimal(type) / 2
+        
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
 }
