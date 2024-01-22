dfc4a6ce5c967e4ce95af589fe0749d45f8670fb
Pascal Sauer <email@cubexs.dev>
2024-01-22 17:21:52 +0100

    Add basic code for project 11

---
+++ Day 53 - Day 56 [Project 11]/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Pascal Sauer on 22.01.24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 53 - Day 56 [Project 11]/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Pascal Sauer on 22.01.24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hello")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
