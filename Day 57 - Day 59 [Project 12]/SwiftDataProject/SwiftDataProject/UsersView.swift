4bdebe8a7da0238b9509627ba54ceec60f492668
Pascal Sauer <email@cubexs.dev>
2024-01-28 03:38:11 +0100

    Update technique project

---
+++ Day 57 - Day 59 [Project 12]/SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -0,0 +1,55 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Pascal Sauer on 28.01.24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct UsersView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var users: [User]
+
+    var body: some View {
+        List(users) { user in
+            HStack {
+                Text(user.name)
+                
+                Spacer()
+                
+                Text(String(user.unwrappedJobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .clipShape(.capsule)
+            }
+        }
+        .onAppear(perform: addSample)
+    }
+    
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]){
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: sortOrder)
+    }
+    
+    func addSample() {
+        let user1 = User(name: "Piper Chapman", city: "New York", joinDate: .now)
+        let job1 = Job(name: "Organize sock drawer", priority: 3)
+        let job2 = Job(name: "Make plans with Alex", priority: 4)
+        
+        modelContext.insert(user1)
+
+        user1.jobs?.append(job1)
+        user1.jobs?.append(job2)
+    }
+}
+
+#Preview {
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
+        .modelContainer(for: User.self)
+}
