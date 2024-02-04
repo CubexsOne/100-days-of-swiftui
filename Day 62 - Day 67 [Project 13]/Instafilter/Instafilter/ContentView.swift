6d5c6915c8bb43df9c5280f7a96ea7ab3d410522
Pascal Sauer <email@cubexs.dev>
2024-02-04 19:06:48 +0100

    Implement basic functionality

--- Day 62 - Day 67 [Project 13]/Instafilter/Instafilter/ContentView.swift
+++ Day 62 - Day 67 [Project 13]/Instafilter/Instafilter/ContentView.swift
@@ -5,28 +5,81 @@
 //  Created by Pascal Sauer on 01.02.24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
 
     var body: some View {
-        VStack {
-            Button("Hello, world!") {
-                showingConfirmation.toggle()
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // Share the picture
+                }
+                
             }
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red}
-                Button("Green") { backgroundColor = .green}
-                Button("Blue") { backgroundColor = .blue}
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color.")
-            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
         }
+    }
+    
+    func changeFilter() {
+        
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
 
