e946567a98024c8d457ad9297d568a06ad68b8d6
Pascal Sauer <email@cubexs.dev>
2024-02-23 21:09:27 +0100

    Initial commit für Project 16

---
+++ Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Pascal Sauer on 23.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedTab = "One"
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Button("Show Tab 2") {
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            .tag("One")
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Pascal Sauer on 23.02.24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
