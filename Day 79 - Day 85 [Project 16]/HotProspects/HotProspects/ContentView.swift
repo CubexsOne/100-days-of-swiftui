131bdac28eee57ae77a2c4cbb1e319ad337a0c46
Pascal Sauer <email@cubexs.dev>
2024-02-25 16:05:23 +0100

    Send local notifications

--- Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ContentView.swift
+++ Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ContentView.swift
@@ -6,32 +6,33 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
-    
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change Color")
-                .padding()
-                .contextMenu {
-                    Button("Red", systemImage: "checkmark.circle.fill") {
-                        backgroundColor = .red
-                    }
-                    .foregroundStyle(.red)
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
                     
-                    Button("Green") {
-                        backgroundColor = .green
-                    }
-                    
-                    Button("Blue") {
-                        backgroundColor = .blue
+                    if success {
+                        print("All set!")
+                    } else if let error {
+                        print(error.localizedDescription)
                     }
                 }
+            }
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
         }
     }
 }
