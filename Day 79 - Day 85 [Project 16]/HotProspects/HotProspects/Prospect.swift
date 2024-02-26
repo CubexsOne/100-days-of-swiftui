7bbbcabbe7dd5b25a374517e1a7bed78e0ff9f62
Pascal Sauer <email@cubexs.dev>
2024-02-26 15:33:57 +0100

    Storing our data with SwiftData

---
+++ Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,22 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Pascal Sauer on 26.02.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Prospect {
+    var name: String
+    var emailAddress: String
+    var isContaced: Bool
+    
+    init(name: String, emailAddress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.isContaced = isContacted
+    }
+}
--- Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ProspectsView.swift
+++ Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Pascal Sauer on 26.02.24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ProspectsView: View {
@@ -12,6 +13,8 @@ struct ProspectsView: View {
         case none, contacted, uncontacted
     }
     
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \Prospect.name) var prospects: [Prospect]
     let filter: FilterType
     
     var title: String {
@@ -26,12 +29,19 @@ struct ProspectsView: View {
     }
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("People: \(prospects.count)")
                 .navigationTitle(title)
+                .toolbar {
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        let prospect = Prospect(name: "Cubi", emailAddress: "cubi@mail.nice", isContacted: false)
+                        modelContext.insert(prospect)
+                    }
+                }
         }
     }
 }
 
 #Preview {
     ProspectsView(filter: .none)
+        .modelContainer(for: Prospect.self)
 }
