b33d6e5d0c0428e5bfcff529639e4b09c40d6240
Pascal Sauer <email@cubexs.dev>
2024-02-26 15:55:11 +0100

    Dynamically filtering our SwiftData query

--- Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ProspectsView.swift
+++ Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ProspectsView.swift
@@ -27,16 +27,37 @@ struct ProspectsView: View {
             "Uncontacted People"
         }
     }
+
     var body: some View {
         NavigationStack {
-            Text("People: \(prospects.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    Button("Scan", systemImage: "qrcode.viewfinder") {
-                        let prospect = Prospect(name: "Cubi", emailAddress: "cubi@mail.nice", isContacted: false)
-                        modelContext.insert(prospect)
-                    }
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
                 }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button("Scan", systemImage: "qrcode.viewfinder") {
+                    let prospect = Prospect(name: "Cubi", emailAddress: "cubi@mail.nice", isContacted: false)
+                    modelContext.insert(prospect)
+                }
+            }
+        }
+    }
+    
+    init(filter: FilterType) {
+        self.filter = filter
+        
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+            
+            _prospects = Query(filter: #Predicate {
+                $0.isContaced == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
         }
     }
 }
