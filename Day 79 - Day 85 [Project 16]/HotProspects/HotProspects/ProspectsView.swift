c76177a28bf21cf5c9d0b9f5472916c66cbe3137
Pascal Sauer <email@cubexs.dev>
2024-02-27 18:51:10 +0100

    Scanning QR codes with SwiftUI

--- Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ProspectsView.swift
+++ Day 79 - Day 85 [Project 16]/HotProspects/HotProspects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Pascal Sauer on 26.02.24.
 //
 
+import CodeScanner
 import SwiftData
 import SwiftUI
 
@@ -15,6 +16,7 @@ struct ProspectsView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @State private var isShowingScanner = false
     let filter: FilterType
     
     var title: String {
@@ -42,9 +44,14 @@ struct ProspectsView: View {
             .navigationTitle(title)
             .toolbar {
                 Button("Scan", systemImage: "qrcode.viewfinder") {
-                    let prospect = Prospect(name: "Cubi", emailAddress: "cubi@mail.nice", isContacted: false)
-                    modelContext.insert(prospect)
+                    isShowingScanner = true
                 }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(
+                    codeTypes: [.qr],
+                    simulatedData: "Cubus Maximus\nCubus@Maximus.dev",
+                    completion: handleScan)
             }
         }
     }
@@ -58,6 +65,21 @@ struct ProspectsView: View {
             _prospects = Query(filter: #Predicate {
                 $0.isContaced == showContactedOnly
             }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+            modelContext.insert(person)
+
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
         }
     }
 }
