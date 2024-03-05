06f1564f3ecd4c2a469aa9d6379ac9f471681086
Pascal Sauer <email@cubexs.dev>
2024-03-05 16:52:29 +0100

    Create Basic App

---
+++ Day 86 - Day 91 [Project 17]/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Pascal Sauer on 05.03.24.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var input: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", input: "Jodie Whittaker")
+}
