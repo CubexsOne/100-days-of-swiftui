25ff9d8f06273e615411cfe71698a7304a589437
Pascal Sauer <email@cubexs.dev>
2024-03-05 16:52:33 +0100

    Designing a single card view

---
+++ Day 86 - Day 91 [Project 17]/Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Pascal Sauer on 03.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView(card: .example)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 86 - Day 91 [Project 17]/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Pascal Sauer on 03.03.24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
