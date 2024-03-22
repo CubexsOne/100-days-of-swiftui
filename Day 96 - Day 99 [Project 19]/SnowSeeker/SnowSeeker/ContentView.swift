dcf0b4cb5a7341b8405e84916f4ba69e88440855
Pascal Sauer <email@cubexs.dev>
2024-03-22 16:59:12 +0100

    Searching for data in a List

--- Day 96 - Day 99 [Project 19]/SnowSeeker/SnowSeeker/ContentView.swift
+++ Day 96 - Day 99 [Project 19]/SnowSeeker/SnowSeeker/ContentView.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     let resorts: [Resort] = Resort.allResorts
+    
+    @State private var searchText = ""
+    
+    var filteredResorts: [Resort] {
+        if searchText.isEmpty {
+            resorts
+        } else {
+            resorts.filter { $0.name.localizedStandardContains(searchText) }
+        }
+    }
 
     var body: some View {
         NavigationSplitView {
-            List(resorts) { resort in
+            List(filteredResorts) { resort in
                 NavigationLink(value: resort) {
                     HStack {
                         Image(resort.country)
@@ -39,6 +49,7 @@ struct ContentView: View {
             .navigationDestination(for: Resort.self) { resort in
                 ResortView(resort: resort)
             }
+            .searchable(text: $searchText, prompt: "Search for a resort")
         } detail: {
             WelcomeView()
         }
