fc23ce1e797b9ee69d096488092a87dc85c42512
Pascal Sauer <email@cubexs.dev>
2024-03-23 10:46:57 +0100

    Add sorting for resort list

--- Day 96 - Day 99 [Project 19]/SnowSeeker/SnowSeeker/ContentView.swift
+++ Day 96 - Day 99 [Project 19]/SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,22 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Sorting {
+        case none, alphabetical, country
+    }
+
     let resorts: [Resort] = Resort.allResorts
     
     @State private var favorites = Favorites()
     @State private var searchText = ""
+    @State private var sortingOrder = Sorting.none
     
-    var filteredResorts: [Resort] {
+    var filteredAndSortedResorts: [Resort] {
+        var filteredResorts: [Resort] = []
         if searchText.isEmpty {
-            resorts
+            filteredResorts = resorts
         } else {
-            resorts.filter { $0.name.localizedStandardContains(searchText) }
+            filteredResorts = resorts.filter { $0.name.localizedStandardContains(searchText) }
+        }
+        
+        return filteredResorts.sorted { valA, valB in
+            switch sortingOrder {
+            case .none:
+                false
+            case .alphabetical:
+                valA.name < valB.name
+            case .country:
+                valA.country < valB.country
+            }
         }
     }
 
     var body: some View {
         NavigationSplitView {
-            List(filteredResorts) { resort in
+            List(filteredAndSortedResorts) { resort in
                 NavigationLink(value: resort) {
                     HStack {
                         Image(resort.country)
@@ -59,6 +76,20 @@ struct ContentView: View {
                 ResortView(resort: resort)
             }
             .searchable(text: $searchText, prompt: "Search for a resort")
+            .toolbar {
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortingOrder) {
+                        Text("Default")
+                            .tag(Sorting.none)
+
+                        Text("Alphabetical")
+                            .tag(Sorting.alphabetical)
+                        
+                        Text("Country")
+                            .tag(Sorting.country)
+                    }
+                }
+            }
         } detail: {
             WelcomeView()
         }
